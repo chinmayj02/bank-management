fed1e4d3d306d511cfdbe3bc42f7b5c77e75751c
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-11-22 13:55:42 +0530

    ignore common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "root", "bank-management") or die(mysqli_error($conn));
+    $conn = mysqli_connect("localhost", "root", "", "bank-management") or die(mysqli_error($conn));
     if(!isset($_SESSION['email'])){
         session_start();
     }

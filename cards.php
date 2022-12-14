e7609d9b6214454f903b698d07c3251351be2a0a
Chinmay Joshi <chinmayujoshi@gmail.com>
2022-12-14 11:04:05 +0530

    cards.php complete

--- cards.php
+++ cards.php
@@ -13,13 +13,6 @@ $short_name = $row['fname'] . " " . $row['lname'];
 $name = $row['fname'] . " " . $row['mname'] . " " . $row['lname'];
 
 
-
-$fetch1 = "select total_balance from total_balance where cin = '{$_SESSION['cin']}'";
-$submit1 = mysqli_query($conn, $fetch1) or die(mysqli_error($conn));
-$row1 = mysqli_fetch_array($submit1);
-// $total_balance = moneyFormatIndia($row1['total_balance']);
-$total_balance =round($row1['total_balance'],2);
-
 $fetch2 = "select balance from account_details join account on account.acc_no=account_details.acc_no where cin = '{$_SESSION['cin']}' and acc_type='SAVINGS'";
 $submit2 = mysqli_query($conn, $fetch2) or die(mysqli_error($conn));
 $row2 = mysqli_fetch_array($submit2);
@@ -71,51 +64,55 @@ $debit = mysqli_fetch_array($submit5);
 
 ?>
 
-<html>
 
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link href="css/cust_dash.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="css/cust_dash.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <link href="css/styles.css" rel="stylesheet">
-
-    <title>DBMS Bank</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/8afb14104e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
 </head>
-
 <body>
-
-    <nav class="navbar position-sticky top-0 left-0 navbar-expand-md">
-        <div class="container-fluid mx-2">
+    
+        <nav class="navbar position-sticky top-0 left-0 navbar-expand-md">
+            <div class="container-fluid mx-2">
+                <div class="navbar-header">
+                    <i data-show="show-side-navigation1" class="show-side-btn fa-fw text-white "></i>
+                    <a class="navbar-brand" href="admin_dashboard.php">DBMS<span class="main-color">Bank</span></a>
+                </div>
+            </div>
+            <div class="container-fluid" style="margin-right: -1060px;">
             <div class="navbar-header">
-                <i data-show="show-side-navigation1" class="show-side-btn fa-fw text-white "></i>
-                <a class="navbar-brand" href="admin_dashboard.php">DBMS<span class="main-color">Bank</span></a>
-            </div>
+				<a type="submit" onclick="history.back()" class="btn btn-success">Go Back</a>
+			</div>
         </div>
-    </nav>
-
-    <aside class="sidebar position-fixed top-40 left-0 overflow-auto h-100 float-left" id="show-side-navigation1">
-        <div class="sidebar-header d-flex justify-content-center align-items-center px-3 py-4">
-            <div class="ms-2">
-                <h5 class="fs-6 mb-0">
-                    <a class="text-decoration-none" href="profile.php">
-                        <?php echo $short_name; ?>
-                    </a>
-                </h5>
-                <p class="mt-1 mb-0">
-                    <?php echo $email; ?>
-                </p>
+        </nav>
+    
+        <aside class="sidebar position-fixed top-40 left-0 overflow-auto h-100 float-left" id="show-side-navigation1">
+            <div class="sidebar-header d-flex justify-content-center align-items-center px-3 py-4">
+                <div class="ms-2">
+                    <h5 class="fs-6 mb-0">
+                        <a class="text-decoration-none" href="profile.php">
+                            <?php echo $short_name; ?>
+                        </a>
+                    </h5>
+                    <p class="mt-1 mb-0">
+                        <?php echo $email; ?>
+                    </p>
+                </div>
             </div>
-        </div>
-
-        <ul class="list-unstyled">
-            <li class="highlight">
+            <ul class="list-unstyled">
+            <li>
                 <a href="customer_dashboard.php"><i class="fa-solid fa-house fa-fw"></i> Dashboard</a>
             </li>
-            <li>
+            <li class="highlight">
                 <a href="cards.php"><i class="fa-regular fa-credit-card fa-fw"></i> Card Details</a>
             </li>
             <!-- <li>
@@ -131,44 +128,8 @@ $debit = mysqli_fetch_array($submit5);
                 <a href="logout.php"><i class="fa-solid fa-right-from-bracket fa-fw"></i> Log Out</a>
             </li>
         </ul>
-    </aside>
-
-    <section id="wrapper">
-        <div class="p-4">
-            <div class="welcome">
-                <div class="content rounded-3 p-3">
-                    <h1 class="fs-3">Hello
-                        <?php echo $name; ?>
-                    </h1>
-                    <p class="mb-0">Welcome to your awesome dashboard!</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="main-skills">
-                <div class="card">
-                    <i class="fa-solid fa-equals"></i>
-                        <h3>Total Balance</h3>
-                        <p><?php echo $total_balance; ?><span><i class="fa-solid fa-indian-rupee-sign"></i></span></p>
-                        <button>Details</button>
-                    </i>
-                </div>
-                <div class="card">
-                    <i class="fa-regular fa-circle-user"></i>
-                        <h3>S/B Account</h3>
-                        <p><?php echo $savings_balance; ?><span><i class="fa-solid fa-indian-rupee-sign"></i></span></p>
-                        <button>Details</button>
-                    </i>
-                </div>
-                <div class="card">
-                    <i class="fa-regular fa-circle-user"></i>
-                        <h3>Current Account</h3>
-                        <p><?php echo $current_balance; ?><span><i class="fa-solid fa-indian-rupee-sign"></i></span></p>
-                        <button>Details</button>
-                    </i>
-                </div>
-               
-            </div>
+        </aside>
+        <section id="wrapper">
             <div class="customer-cards">
                 <div class="card-1">
                 <div class="CARD">
@@ -228,10 +189,6 @@ $debit = mysqli_fetch_array($submit5);
                     </div>
                 </div>
             </div>
-    </section>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.js'></script>
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
-        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+        </section>
 </body>
-
 </html>
